c1b49f0af04515b236d4f72478874d7669a461b7
MagTheDev <jtulek@at-rt.com>
2023-01-15 19:18:26 +0100

    Add vue plugin for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    site: 'https://magthe.dev',
-    vite: {
-        ssr: {
-            external: ["svgo"]
-        }
+  output: 'server',
+  site: 'https://magthe.dev',
+  vite: {
+    ssr: {
+      external: ["svgo"]
     }
+  },
+  integrations: [vue()]
 });

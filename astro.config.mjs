be63b55719d602970d3590a852def42adf31493c
Jakub Tulek <jakub.tulek@esmosk.sk>
2025-07-08 19:08:08 +0200

    Add font-display property to all Berkeley Mono font faces

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  site: 'https://magthe.dev',
-  vite: {
-    ssr: {
-      external: ["svgo"]
-    }
-  },
-  integrations: [vue()]
+	site: 'https://magthe.dev',
+	vite: {
+		ssr: {
+			external: ["svgo"]
+		},
+	},
 });
